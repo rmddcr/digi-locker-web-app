fcf2b65f7b8c8c71dd644bbe78f56d7f29ed3c2d
Damitha Ranasinghe <rmddcr@gmail.com>
2018-05-22 02:56:29 +0530

    Add role view

--- application/views/role/all_roles.php
+++ application/views/role/all_roles.php
@@ -1,1 +1,27 @@
-all_roles
+<?php var_dump($result_array[0]);?>
+
+<?php foreach ($result_array as $row) : ?>
+
+    <div class="card">
+
+
+        <table id="lockers_current_table" class="display">
+
+            <tbody>
+            <tr>
+                <td><?php echo $row['role_name'] ; ?></td>
+                <td><form action="<?php echo base_url()."User/view/";?>">
+                        <input type="submit"  class="btn btn-info m-b-10 m-l-5" value="Edit privileges" />
+                    </form>
+                </td>
+
+            </tr>
+            </tbody>
+        </table>
+
+
+    </div>
+
+
+<?php endforeach;?>
+
