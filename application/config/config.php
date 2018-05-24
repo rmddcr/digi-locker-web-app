d6a804a2d36dea9f7ec8c3f9fef9bd2c2344b8e5
Damitha Ranasinghe <rmddcr@gmail.com>
2018-05-24 11:52:42 +0530

    edit .gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://159.65.227.196/digi-locker-web-app/';
+$config['base_url'] = 'http://localhost:8081/';
 
 /*
 |--------------------------------------------------------------------------
