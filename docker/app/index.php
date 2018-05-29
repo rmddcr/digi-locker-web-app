79e2744fefb9bd685122a96eec1701389cab82f1
Damitha Ranasinghe <rmddcr@gmail.com>
2018-05-29 11:17:13 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ docker/app/index.php
@@ -0,0 +1,5 @@
+<html>
+ <body>
+ <?php echo '<p>Hello World this is a folder for docker</p>'; ?> 
+ </body>
+</html>
--- start-script.sh
+++ start-script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+chmod 777 /var/www/html
 service mysql start
 a2enmod rewrite
 service apache2 start
+systemctl restart apache2
